0898c079eb0c30d30bb509b28450a7afb9d8c81c
Kovi <gabriel.de.kovacs@gmail.com>
2017-08-02 17:20:51 +0200

    udp client test phase

---
+++ Projects/robot_arm/SW4STM32/STM32746G_DISCOVERY/Application/User/client.c
@@ -0,0 +1,98 @@
+#include "client.h"
+/* Includes ------------------------------------------------------------------*/
+#include "stm32746g_discovery_ts.h"
+#include "lcd_log.h"
+#include "cmsis_os.h"
+#include "app_ethernet.h"
+#include "lwip/sockets.h"
+#include "stm32746g_discovery_ts.h"
+#include <string.h>
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+#define CLIENT_SERVER_IP			"10.27.6.186"
+#define UDP_SERVER_PORT					54545
+
+void udp_client_thread(void const *argument)
+{
+	udp_client_ready = 0;
+	udp_send_allowed = 1;
+
+	// Create a new socket to listen for client connections.
+	int udp_client_socket;
+
+	udp_client_socket = lwip_socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+	if (udp_client_socket < 0) {
+		LCD_ErrLog((char*) "Cannot create UDP client socket.\n");
+		LCD_ErrLog((char*) "Closing application.\n");
+		osThreadTerminate(NULL);
+	} else {
+		LCD_UsrLog((char*) "UDP client socket is up.\n");
+	}
+
+	// Create client address structure
+		struct sockaddr_in udp_client_addr;
+		udp_client_addr.sin_family = AF_INET;
+		int udp_client_addr_size = sizeof(udp_client_addr);
+
+		// Create remote server address structure
+		struct sockaddr_in udp_remote_addr;
+		udp_remote_addr.sin_family = AF_INET;
+		udp_remote_addr.sin_addr.s_addr = INADDR_BROADCAST;
+		udp_remote_addr.sin_port = htons(UDP_SERVER_PORT);
+
+
+		 // Set socket to broadcast
+		int opt_val = 1;
+		int opt_len = sizeof(int);
+		int set_broadcast = lwip_setsockopt(udp_client_socket, SOL_SOCKET,
+										    SO_BROADCAST, &opt_val, (socklen_t) opt_len);
+		if (set_broadcast < 0) {
+				LCD_ErrLog((char*) "Cannot set UDP client to broadcast.\n");
+				LCD_ErrLog((char*) "Closing application.\n");
+				lwip_close(udp_client_socket);
+				osThreadTerminate(NULL);
+			} else {
+				LCD_UsrLog((char*) "UDP client is ready to broadcast.\n");
+			}
+
+		udp_client_ready = 1;
+
+		LCD_UsrLog((char*) "UDP client is ready.\n");
+		char buff[128];
+		char message[] = "time1.google.com";
+
+		LCD_UsrLog("%s\n", message);
+
+		LCD_UsrLog("Socket client - startup...\n");
+		LCD_UsrLog("Socket client - waiting for IP address...\n");
+
+		// Wait for an IP address
+		while (!is_ip_ok())
+			osDelay(10);
+
+		// Try to connect to the server
+		// Send udp package
+		sendto(udp_client_socket, message, strlen(message),
+								 0, (struct sockaddr*) &udp_remote_addr, sizeof(udp_remote_addr));
+		LCD_UsrLog("Socket client - data sent\n");
+
+		int recv_bytes = recvfrom(udp_client_socket, buff, sizeof(buff),
+								   0, (struct sockaddr*) &udp_client_addr,
+								   (socklen_t*) &udp_client_addr_size);
+		if (recv_bytes > 0) {
+			LCD_UsrLog("Socket client - data received: \n");
+			buff[recv_bytes] = '\0';
+			LCD_UsrLog(buff);
+			LCD_UsrLog("\n");
+		}
+
+
+	    // Close socket
+	    lwip_close(udp_client_socket);
+
+	    // Close thread
+	    while (1) {
+	    	osThreadTerminate(NULL);
+	    }
+}
