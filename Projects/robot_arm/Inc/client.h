d54ef5761ec93aed962831aeca6c7ee7436255eb
Kovi <gabriel.de.kovacs@gmail.com>
2017-08-02 13:32:34 +0200

    rewrite client to udp

--- Projects/robot_arm/Inc/client.h
+++ Projects/robot_arm/Inc/client.h
@@ -1,17 +1,14 @@
 #ifndef __CLIENT_H_
 #define __CLIENT_H_
 
-#include "stm32f7xx_hal.h"
+#include <string.h>
+#include <stdint.h>
+#include <math.h>
 #include "cmsis_os.h"
-#include "lcd_log.h"
-#include "stm32f7xx_hal.h"
-#include "stm32746g_discovery.h"
-#include "stm32746g_discovery_lcd.h"
-#include "lcd_log.h"
-#include "cmsis_os.h"
-#include "app_ethernet.h"
-#include "lwip/sockets.h"
 
-void socket_client_thread(void const *argument);
+int udp_client_ready;
+int udp_send_allowed;
+
+void udp_client_thread(void const *argument);
 
 #endif // __CLIENT_H_
