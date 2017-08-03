0e51e647a3aa912cd47ce07b32af4b3d62272fb3
Kovi <gabriel.de.kovacs@gmail.com>
2017-08-03 13:42:55 +0200

    struct for the time

--- Projects/robot_arm/SW4STM32/STM32746G_DISCOVERY/Application/User/client.c
+++ Projects/robot_arm/SW4STM32/STM32746G_DISCOVERY/Application/User/client.c
@@ -7,11 +7,43 @@
 #include "lwip/sockets.h"
 #include "stm32746g_discovery_ts.h"
 #include <string.h>
-
 /* Private typedef -----------------------------------------------------------*/
 /* Private define ------------------------------------------------------------*/
 #define CLIENT_SERVER_IP			"216.239.32.15"
-#define UDP_SERVER_PORT					123
+#define UDP_SERVER_PORT					80
+
+typedef struct
+	{
+
+		unsigned li   : 2;       // Only two bits. Leap indicator.
+		unsigned vn   : 3;       // Only three bits. Version number of the protocol.
+		unsigned mode : 3;       // Only three bits. Mode. Client will pick mode 3 for client.
+
+		uint8_t stratum;         // Eight bits. Stratum level of the local clock.
+		uint8_t poll;            // Eight bits. Maximum interval between successive messages.
+		uint8_t precision;       // Eight bits. Precision of the local clock.
+
+		uint32_t rootDelay;      // 32 bits. Total round trip delay time.
+		uint32_t rootDispersion; // 32 bits. Max error aloud from primary clock source.
+		uint32_t refId;          // 32 bits. Reference clock identifier.
+
+		uint32_t refTm_s;        // 32 bits. Reference time-stamp seconds.
+		uint32_t refTm_f;        // 32 bits. Reference time-stamp fraction of a second.
+
+		uint32_t origTm_s;       // 32 bits. Originate time-stamp seconds.
+		uint32_t origTm_f;       // 32 bits. Originate time-stamp fraction of a second.
+
+		uint32_t rxTm_s;         // 32 bits. Received time-stamp seconds.
+		uint32_t rxTm_f;         // 32 bits. Received time-stamp fraction of a second.
+
+		uint32_t txTm_s;         // 32 bits and the most important field the client cares about. Transmit time-stamp seconds.
+		uint32_t txTm_f;         // 32 bits. Transmit time-stamp fraction of a second.
+
+	} ntp_packet;                 // Total: 384 bits or 48 bytes.
+
+	// Create and zero out the packet. All 48 bytes worth.
+
+	ntp_packet packet = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 
 void udp_client_thread(void const *argument)
 {
@@ -41,6 +73,7 @@ void udp_client_thread(void const *argument)
 		udp_remote_addr.sin_addr.s_addr = INADDR_BROADCAST;
 		udp_remote_addr.sin_port = htons(UDP_SERVER_PORT);
 
+		memset( &packet, 0, sizeof( ntp_packet ) );
 
 		 // Set socket to broadcast
 		int opt_val = 1;
