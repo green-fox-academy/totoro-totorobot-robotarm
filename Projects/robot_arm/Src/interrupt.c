2e9d04fba75c50a32d5f78499c5df7f966dfa893
VargaElod <varga.elod@gmail.com>
2017-08-22 17:07:49 +0200

    set pins and modify functions

--- Projects/robot_arm/Src/interrupt.c
+++ Projects/robot_arm/Src/interrupt.c
@@ -2,9 +2,14 @@
 
 #define END_STOP1		GPIO_PIN_6
 #define END_STOP2		GPIO_PIN_6
+#define POWER_ON		GPIO_PIN_7
+#define END_STOP_POWER	GPIO_PIN_7
 
 void end_stop_init(void)
 {
+	HAL_GPIO_WritePin(GPIOC, END_STOP_POWER, GPIO_PIN_SET);
+	HAL_GPIO_WritePin(GPIOG, POWER_ON, GPIO_PIN_RESET);
+
 	/* Set Interrupt priority */
 	HAL_NVIC_SetPriority(EXTI0_IRQn, 0, 1);
 
@@ -16,5 +21,6 @@ void end_stop_thread(void const * argument)
 {
 	end_stop_init();
 	HAL_GPIO_EXTI_IRQHandler(END_STOP1);
+
 	EXTI_IRQHandler(void);
 }
