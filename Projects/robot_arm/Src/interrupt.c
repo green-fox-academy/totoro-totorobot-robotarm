f53cb6775d01479bbadb79e47d6d387e593abe04
VargaElod <varga.elod@gmail.com>
2017-08-24 15:44:11 +0200

    change pins

--- Projects/robot_arm/Src/interrupt.c
+++ Projects/robot_arm/Src/interrupt.c
@@ -6,70 +6,82 @@
 
 GPIO_InitTypeDef GPIO_Init;
 
+void EXTI3_IRQHandler_Config(void)
+{
+	// Enable GPIO clocks
+	__HAL_RCC_GPIOI_CLK_ENABLE();
+
+	//END_STOP1
+	//8 D7 PI3
+	GPIO_Init.Pin = GPIO_PIN_3;
+	GPIO_Init.Mode = GPIO_MODE_IT_RISING_FALLING;
+	GPIO_Init.Pull = GPIO_NOPULL;
+	HAL_GPIO_Init(GPIOI, &GPIO_Init);
+
+	/* Set Interrupt priority */
+	HAL_NVIC_SetPriority(EXTI3_IRQn, 0, 1);
+
+	/* Enable  in end_stop_threadterrupt */
+	HAL_NVIC_EnableIRQ(EXTI3_IRQn);
+}
+
+void EXTI2_IRQHandler_Config(void)
+{
+	// Enable GPIO clocks
+	__HAL_RCC_GPIOI_CLK_ENABLE();
+
+	//END_STOP2
+	//1 D8 PI2
+	GPIO_Init.Pin = GPIO_PIN_2;
+	GPIO_Init.Mode = GPIO_MODE_IT_FALLING;
+	GPIO_Init.Pull = GPIO_NOPULL;
+	HAL_GPIO_Init(GPIOI, &GPIO_Init);
+
+	/* Set Interrupt priority */
+	HAL_NVIC_SetPriority(EXTI2_IRQn, 0, 1);
+
+	/* Enable  in end_stop_threadterrupt */
+	HAL_NVIC_EnableIRQ(EXTI2_IRQn);
+}
+
 void pin_init(void)
 {
 	// Enable GPIO clocks
-	__HAL_RCC_GPIOC_CLK_ENABLE();
 	__HAL_RCC_GPIOG_CLK_ENABLE();
 
-	//END_STOP1
-	//2 D1 PC6
-	GPIO_Init.Pin = GPIO_PIN_6;
-	GPIO_Init.Mode = GPIO_MODE_IT_RISING_FALLING;
-	GPIO_Init.Pull = GPIO_NOPULL;
-
-	HAL_GPIO_Init(GPIOC, &GPIO_Init);
-
-	//END_STOP2
-	//3 D2 PG6
-	GPIO_Init.Pin = GPIO_PIN_6;
-	GPIO_Init.Mode = GPIO_MODE_IT_FALLING;
-	GPIO_Init.Pull = GPIO_NOPULL;
-
-	HAL_GPIO_Init(GPIOG, &GPIO_Init);
-
 	// POWER_ON
-	//5 D4 PB4
+	//5 D4 PG7
 	GPIO_Init.Pin = GPIO_PIN_7	;
 	GPIO_Init.Mode = GPIO_MODE_OUTPUT_PP;
 	GPIO_Init.Pull = GPIO_NOPULL;
-
 	HAL_GPIO_Init(GPIOG, &GPIO_Init);
 
 	// Set POWER_ON state
 	HAL_GPIO_WritePin(GPIOG, POWER_ON, GPIO_PIN_RESET);
-
-	/* Set Interrupt priority group*/
-	HAL_NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_2);
-
-	/* Set Interrupt priority */
-	HAL_NVIC_SetPriority(EXTI0_IRQn, 0, 1);
-
-	/* Enable  in end_stop_threadterrupt */
-    HAL_NVIC_EnableIRQ(EXTI0_IRQn);
 }
 
-void EXTI1_IRQHandler(void)
+void EXTI3_IRQHandler(void)
 {
-
-	HAL_GPIO_EXTI_IRQHandler(END_STOP1);
+	HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_3);
 }
 
 void EXTI2_IRQHandler(void)
 {
-	HAL_GPIO_EXTI_IRQHandler(END_STOP2);
+	HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_2);
+}
+
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
+{
+	if ((GPIO_Pin == GPIO_PIN_3) || (GPIO_Pin == GPIO_PIN_2)) {
+		HAL_GPIO_WritePin(GPIOG, POWER_ON, GPIO_PIN_SET);
+	}
 }
 
 void end_stop_thread(void const * argument)
 {
 	pin_init();
 	while (1) {
-		EXTI1_IRQHandler();
-		HAL_GPIO_WritePin(GPIOG, POWER_ON, GPIO_PIN_SET);
-		osDelay(5000);
-		HAL_GPIO_WritePin(GPIOG, POWER_ON, GPIO_PIN_RESET);
-		osDelay(5000);
-		HAL_GPIO_EXTI_Callback(END_STOP1);
+		HAL_GPIO_EXTI_Callback();
 	}
 	//EXTI2_IRQHandler();
 }
